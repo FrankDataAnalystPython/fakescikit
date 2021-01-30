668a9758c77da8f6d30ed34d38c3019d05d741c1
Xiangyang Ni <frank935460794@live.com>
2021-01-30 13:16:54 +0800

    adding a fake GridSearchCV class

--- fakesklearn/common_tools.py
+++ fakesklearn/common_tools.py
@@ -1,7 +1,11 @@
 import numpy as np
 import copy
+import itertools
+import sklearn
 from sklearn.preprocessing import LabelBinarizer
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, recall_score, r2_score
+from sklearn.model_selection import cross_val_score
+from sklearn.base import clone
 
 class OneVsOneClassifier:
     def __init__(self, estimator = None):
@@ -33,3 +37,39 @@ class OneVsOneClassifier:
     def score(self, X, Y):
         Y_pred = self.predict(X)
         return accuracy_score(Y, Y_pred)
+
+
+class fakeSearchCV:
+    def __init__(self,
+                 estimator,
+                 param_grid,
+                 scoring='accuracy',
+                 cv=5):
+        self.estimator = estimator
+        self.param_grid = param_grid
+        self.scoring = scoring
+        self.cv = cv
+
+    def init_params(self):
+        self.all_p = [{k: v for k, v in zip(p, i)}
+                      for i in itertools.product(*self.param_grid.values())]
+        self.base_estimators_ = [clone(self.estimator.set_params(**i))
+                                 for i in self.all_p]
+
+    def fit(self, X, Y):
+        self.init_params()
+        self.Results = []
+        for model in self.base_estimators_:
+            cv_score = cross_val_score(model, X, Y, cv=self.cv, scoring=self.scoring).mean()
+            self.Results.append(cv_score)
+        self.final_df_result = np.array(self.Results)
+        self.best_score_ = self.final_df_result.max()
+        self.best_params_ = self.all_p[self.final_df_result.argmax()]
+        self.best_estimator_ = self.estimator.set_params(**self.best_params_).fit(X, Y)
+        return self
+
+    def predict(self, X):
+        return self.best_estimator_.predict(X)
+
+    def score(self, X, Y):
+        return sklearn.metrics.SCORERS[self.scoring](self.best_estimator_, X, Y)
